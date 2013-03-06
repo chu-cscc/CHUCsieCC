3d42938e32cb95f87ee68d3b74a4ccd887d0badb
ScarWu <xneriscool@gmail.com>
2013-03-07 06:19:38 +0800

    modify js css html

---
+++ public/js/cc.js
@@ -0,0 +1,38 @@
+'use strict';
+/* ScarWu, 2013 */
+
+var setOnNavBottonClick = function () {
+	$('nav a').on('click', function () {
+		event.preventDefault();
+
+		var target = $(this).attr('href');
+		var moveTo = $(target).position().top - $('nav').height();
+
+		$('html, body').stop().animate({ scrollTop: moveTo }, 750);
+	});
+}
+
+$(document).ready(function () {
+	setOnNavBottonClick();
+});
+
+var fixed_nav_active = false;
+$(window).on('scroll', function () {
+	if ($('body').scrollTop() >= $('header').height()) {
+
+		if (!fixed_nav_active) {
+			var nav = $('nav').clone().addClass('fixed');
+
+			$('body').append(nav);
+			setOnNavBottonClick();
+
+			fixed_nav_active = true;
+		}
+
+	}
+	else {
+		$('nav.fixed').remove();
+
+		fixed_nav_active = false;
+	}
+});
