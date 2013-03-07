d808c213752eae2a827a01da4164cd3a56c8cc6b
ScarWu <xneriscool@gmail.com>
2013-03-07 16:04:31 +0800

    update all

--- public/js/cc.js
+++ public/js/cc.js
@@ -14,17 +14,24 @@ var setOnNavBottonClick = function () {
 
 $(document).ready(function () {
 	setOnNavBottonClick();
+	$('#services .info .mirror').siblings().hide();
+});
 
-	$('.top').on('click', function () {
-		event.preventDefault();
-		$('html, body').stop().animate({ scrollTop: 0 }, 750);
-	});
+$('.top').on('click', function () {
+	event.preventDefault();
+	$('html, body').stop().animate({ scrollTop: 0 }, 750);
+});
+
+$('#services .menu .item').on('mouseover', function() {
+	$(this).addClass('active');
+	$(this).siblings().removeClass('active');
+	var target = $(this).attr('class').split(' ')[0];
+	$('#services .info .' + target).show().siblings().hide();
 });
 
 var fixed_nav_active = false;
 $(window).on('scroll', function () {
 	if ($('body').scrollTop() >= $('header').height()) {
-
 		if (!fixed_nav_active) {
 			var nav = $('nav').clone().addClass('fixed');
 
@@ -33,7 +40,6 @@ $(window).on('scroll', function () {
 
 			fixed_nav_active = true;
 		}
-
 	}
 	else {
 		$('nav.fixed').remove();
